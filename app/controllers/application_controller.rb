629d1caeeb595274a33a14291e12e03e4fa29871
NeilTheSeal <neil.hendren@gmail.com>
2024-05-22 10:06:44 -0600

    Add comment with possible credentials check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  # before_action :require_login
 
   def current_user
     @_current_user = User.find(session[:user_id]) if session[:user_id]
   end
+
+  # def require_login
+  #   redirect_to root_path unless current_user
+  # end
 end
