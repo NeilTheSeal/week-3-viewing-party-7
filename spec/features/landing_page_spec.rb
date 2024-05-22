ae5f29f7c85bb6eec718e33d3bb4bba26e7390d1
NeilTheSeal <neil.hendren@gmail.com>
2024-05-21 20:01:29 -0600

    Complete user story 4

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe "Landing Page" do
   before :each do
-    user1 = User.create(
+    @user1 = User.create(
       name: "User One",
       email: "user1@test.com",
       password: "test1"
     )
-    user2 = User.create(
+    @user2 = User.create(
       name: "User Two",
       email: "user2@test.com",
       password: "test2"
@@ -30,23 +30,25 @@ RSpec.describe "Landing Page" do
     expect(current_path).to eq(root_path)
   end
 
-  it "lists out existing users" do
-    user1 = User.create(
-      name: "User One",
-      email: "user1@test.com",
-      password: "password"
-    )
-    user2 = User.create(
-      name: "User Two",
-      email: "user2@test.com",
-      password: "password2"
-    )
+  it "lists out existing users if logged in" do
+    click_link "Log In"
+
+    fill_in :email, with: @user1.email
+    fill_in :password, with: "test1"
+
+    click_on "Log In"
 
     expect(page).to have_content("Existing Users:")
 
     within(".existing-users") do
-      expect(page).to have_content(user1.email)
-      expect(page).to have_content(user2.email)
+      expect(page).to have_content(@user1.email)
+      expect(page).to have_content(@user2.email)
     end
+  end
+
+  it "does not list existing users if not logged in" do
+    expect(page).to_not have_content("Existing Users:")
+    expect(page).to_not have_content(@user1.email)
+    expect(page).to_not have_content(@user2.email)
   end
 end
